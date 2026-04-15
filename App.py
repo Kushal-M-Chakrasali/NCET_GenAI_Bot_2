90033362461b7b6b9b3802934f7750432942b0f0
Kushal-M-Chakrasali <chakrasalikushal@gmail.com>
2026-04-15 14:58:10 +0530

    Update App.py

--- App.py
+++ App.py
@@ -16,5 +16,5 @@ if st.button("Summarize"):
       min_length=min_length, do_sample=False)
       st.subheader(" Summary:")
       st.success(summary[0]['summary_text'])
-  else:
-  st.warning(" Please enter some text to summarize.")
+else:
+st.warning(" Please enter some text to summarize.")
